2ba5de8260c526aa406229a99436b6023fc530b3
Siyi Liu <liusiyi@client0090vag.vpn.private.upenn.edu>
2021-06-09 12:01:51 -0400

    eval bash

--- eval.sh
+++ eval.sh
@@ -3,27 +3,27 @@ echo "BART baseline" >> results/results.txt
 
 for seed in 1 6 9
 do
-python eval_args.py --dataset_path data/MultiOpEd.csv --generated_file outputs/bart_baseline/seed=$seed/test_generated.txt --labels_file outputs/bart_baseline/seed=$seed/test_labels.txt --relevance_classifier_path /path/to/relevance/classifier --stance_classifier_path /path/to/stance/classifier --result_path results
+python eval.py --dataset_path data/MultiOpEd.csv --generated_file outputs/bart_baseline/seed=$seed/test_generated.txt --labels_file outputs/bart_baseline/seed=$seed/test_labels.txt --relevance_classifier_path /path/to/relevance/classifier --stance_classifier_path /path/to/stance/classifier --result_path results
 done
 
 echo "BART+Relevance" >> results/results.txt
 
 for seed in 1 6 9
 do
-python eval_args.py --dataset_path data/MultiOpEd.csv --generated_file outputs/bart+relevance/seed=$seed/test_generated.txt --labels_file outputs/bart+relevance/seed=$seed/test_labels.txt --relevance_classifier_path /path/to/relevance/classifier --stance_classifier_path /path/to/stance/classifier --result_path results
+python eval.py --dataset_path data/MultiOpEd.csv --generated_file outputs/bart+relevance/seed=$seed/test_generated.txt --labels_file outputs/bart+relevance/seed=$seed/test_labels.txt --relevance_classifier_path /path/to/relevance/classifier --stance_classifier_path /path/to/stance/classifier --result_path results
 done
 
 echo "BART+stance" >> results/results.txt
 
 for seed in 1 6 9
 do
-python eval_args.py --dataset_path data/MultiOpEd.csv --generated_file outputs/bart+stance/seed=$seed/test_generated.txt --labels_file outputs/bart+stance/seed=$seed/test_labels.txt --relevance_classifier_path /path/to/relevance/classifier --stance_classifier_path /path/to/stance/classifier --result_path results
+python eval.py --dataset_path data/MultiOpEd.csv --generated_file outputs/bart+stance/seed=$seed/test_generated.txt --labels_file outputs/bart+stance/seed=$seed/test_labels.txt --relevance_classifier_path /path/to/relevance/classifier --stance_classifier_path /path/to/stance/classifier --result_path results
 done
 
 
 echo "BART+both" >> results/results.txt
 for seed in 1 6 9
 do
-python eval_args.py --dataset_path data/MultiOpEd.csv --generated_file outputs/bart+both/seed=$seed/test_generated.txt --labels_file outputs/bart+both/seed=$seed/test_labels.txt --relevance_classifier_path /path/to/relevance/classifier --stance_classifier_path /path/to/stance/classifier --result_path results
+python eval.py --dataset_path data/MultiOpEd.csv --generated_file outputs/bart+both/seed=$seed/test_generated.txt --labels_file outputs/bart+both/seed=$seed/test_labels.txt --relevance_classifier_path /path/to/relevance/classifier --stance_classifier_path /path/to/stance/classifier --result_path results
 done
 
